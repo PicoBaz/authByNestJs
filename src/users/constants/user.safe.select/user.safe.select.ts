7a595ab96e45b10b79d069b78dc05438ae9c1837
PicoBaz <picobaz3@gmail.com>
2026-07-01 16:50:52 +0330

    feat(const) : generate user safe select const

---
+++ src/users/constants/user.safe.select/user.safe.select.ts
@@ -0,0 +1,11 @@
+export const UserSafeSelect = {
+  id: true,
+  uuid: true,
+  email: true,
+  firstName: true,
+  lastName: true,
+  phone: true,
+  status: true,
+  createdAt: true,
+  updatedAt: true,
+} as const;
